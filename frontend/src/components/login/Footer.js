7c5ef500ce878916a187184136d7caa172df8c02
Akarsh711 <paliwalap7@gmail.com>
2023-06-13 09:22:53 +0530

    added login page frontend

---
+++ frontend/src/components/login/Footer.js
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+
+export default function Footer() {
+  return (
+    <footer className="login_footer">
+          <div className="login_footer_wrap">
+            <Link to="/">English(UK)</Link>
+            <Link to="/">Fran</Link>
+            <Link to="/">हिन्दी </Link>
+            <Link to="/">اردو </Link>
+            <Link to="/">ਪੰਜਾਬੀ </Link>
+            <Link to="/">বাংলা </Link>
+            <Link to="/">ગુજરાતી </Link>
+            <Link to="/">मराठी </Link>
+            <Link to="/">தமிழ்</Link>
+            <Link to="/">తెలుగు</Link>
+            <Link to="/">മലയാളം </Link>
+            <Link to="/">ಕನ್ನಡ </Link>
+            <Link to="/" className="footer_square">
+              <i className="plus_icon"></i>
+            </Link>
+          </div>
+          <div className="footer_splitter"></div>
+          <div className="login_footer_wrap">
+            <Link to="/">Sign Up</Link>
+            <Link to="/">Log In </Link>
+            <Link to="/">Messenger  </Link>
+            <Link to="/">Facebook Lite </Link>
+            <Link to="/">Watch</Link>
+            <Link to="/">Places</Link>
+            <Link to="/">Games</Link>
+            <Link to="/">Marketplace</Link>
+            <Link to="/">Meta Pay</Link>
+            <Link to="/">Meta Store</Link>
+            <Link to="/">Meta Quest</Link>
+            <Link to="/">Instagram</Link>
+            <Link to="/">Fundraisers</Link>
+            <Link to="/">Services</Link>
+            <Link to="/">Voting</Link>
+            <Link to="/">Information Center</Link>
+            <Link to="/">Privacy Policy</Link>
+            <Link to="/">Privacy Centre</Link>
+            <Link to="/">Groups</Link>
+            <Link to="/">About</Link>
+            <Link to="/">Create ad</Link>
+            <Link to="/">Create Page</Link>
+            <Link to="/">Developers</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Cookies</Link>
+            <Link to="/">
+              Ad Choices
+              <i className="adChoices_icon"></i>
+            </Link>
+            <Link to="/">Terms</Link>
+            <Link to="/">Help</Link>
+            <Link to="/">Contact uploading and non-users</Link>
+            <Link to="/">Settings</Link>
+            <Link to="/">Activity</Link>
+            <Link to="/">log</Link>
+            Meta © 2023
+          </div>
+        </footer>
+  )
+}
--- frontend/src/pages/login/index.js
+++ frontend/src/pages/login/index.js
@@ -1,44 +1,19 @@
-// import React from 'react'
-import './style.css'
-import {Formik, Form} from "formik";
-import {Link} from "react-router-dom";
-import LoginInput from '../../components/inputs/loginInput';
+import "./style.css";
+import LoginForm from "../../components/login/LoginForm";
+import Footer from "../../components/login/Footer";
+
+
+
+
 export default function Login() {
+
   return (
     <div className="login">
       <div className="login_wrapper">
-        <div className="login_wrap">
-          <div className="login_1">
-            <img src="../../icons/facebook.svg" alt=""/>
-            <span>
-              facebook helps you connet and share with people in your life
-            </span>
-          </div>
-          <div className="login_2">
-            <div className="login_2_wrap">
-              <Formik>
-                {
-                  (formik)=>(
-                    <Form>
-                      <LoginInput />
-                      <LoginInput />
-                      <button type="submit" className="blue_btn">Logi In</button>
-                    </Form>
-                  )
-                }
-              </Formik>
-              <Link to="/forgot" className="forgot_password">Forgotten password?</Link>
-              <div className="sign_splitter"></div>
-              <button className="blue_btn open_signup">Create Account</button>
-            </div>
-            <Link to="/" className="sign_extra">
-              <b>Create A page</b>
-                for a celeberity, brand or buisneess.
-            </Link>
-          </div>
-        </div>
+        <LoginForm/>
+        <div className="register"></div>
+        <Footer/>
       </div>
-      <div className="register"></div>
     </div>
-  )
+  );
 }

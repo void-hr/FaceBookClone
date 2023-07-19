498cb7f89d35bc21f2fd1fd2136938dd7ea1f4ba
Akarsh711 <paliwalap7@gmail.com>
2023-07-19 21:36:31 +0530

    done with lec 54 added left side bar

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -1,17 +1,21 @@
 import Header from "../../components/header";
 import { useState, useRef } from "react";
 import useClickOutSide from "../../helpers/clickOutside";
+import LeftHome from "../../components/home/left";
+import { useSelector } from "react-redux";
 
 export default function Home() {
-	const [visible, setVisible] = useState(true);
-	const el = useRef(null);
-	useClickOutSide(el, () => {
-		setVisible(false);
-	});
+	// const [visible, setVisible] = useState(true);
+	// const el = useRef(null);
+	// useClickOutSide(el, () => {
+	// 	setVisible(false);
+	// });
+	const {user} = useSelector((user)=>({...user})); 
 	return (
 		<div>
 			<Header />
-			{visible && <div className="card" ref={el}></div>}
+			<LeftHome user={user}/>
+			{/* {visible && <div className="card" ref={el}></div>} */}
 		</div>
 	);
 }

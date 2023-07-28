a2760915d30870e3b5f0df571a336f555ea52b60
void-hr <gsharmaji93@gmail.com>
2023-07-28 21:18:48 +0530

    check

--- frontend/src/pages/home/activate.js
+++ frontend/src/pages/home/activate.js
@@ -1,6 +1,7 @@
 import Header from "../../components/header";
-import { useState, useRef, useEffect } from "react";
-import useClickOutSide from "../../helpers/clickOutside";
+import { useState, useEffect } from "react";
+// import { useRef } from "react";
+// import useClickOutSide from "../../helpers/clickOutside";
 import LeftHome from "../../components/home/left";
 import { useDispatch, useSelector } from "react-redux";
 import RightHome from "../../components/home/right";
@@ -9,9 +10,8 @@ import "./style.css";
 import CreatePost from "../../components/createPost";
 import ActivateForm from "./ActivateForm";
 import axios from "axios";
-import Cookie from "js-cookie";
+// import Cookie from "js-cookie";
 import { useNavigate, useParams } from "react-router-dom";
-
 
 export default function Activate() {
 	// const [visible, setVisible] = useState(true);
@@ -20,71 +20,67 @@ export default function Activate() {
 	// 	setVisible(false);
 	// });
 	const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const { user } = useSelector((user) => ({...user}));
-    const {token} = useParams();
-    const [success, setSuccess] = useState();
-    const [error, setError] = useState("");
-    const [loading, setLoading] = useState(true);
+	const navigate = useNavigate();
+	const { user } = useSelector((user) => ({ ...user }));
+	const [success, setSuccess] = useState();
+	const [error, setError] = useState("");
+	const [loading, setLoading] = useState(true);
+	const { token } = useParams();
 
-    useEffect(()=>{
-        activateAccount();
-    }, []);
-    
-    if(user){
-        console.log(user);
-    }else{
-        console.log("elese")
-    }
-    const activateAccount = async ()=>{
-        try{
-            setLoading(true);
-            const {data} = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/activate`,{token},{
-                headers:{
-                    Authorization: `Bearer ${user.token}`,
-                }
-            });
+	console.log(token);
 
-            setSuccess(data.message);
-            Cookie.set('user', JSON.stringify({...user, verified: true}));
-            dispatch({
-                type:'VERIFY', 
-                payload: true,
-            })
-            setTimeout(()=>{
-                navigate("/")
-            },3000);
-            setLoading(false);
-        }catch(error){
-            console.log(error.response.data);
-            setError(error.response.data.message);
-            setTimeout(()=>{
-                navigate("/")
-            },3000);
-        }
-    }
-    
+	const activateAccount = async () => {
+		try {
+			setLoading(true);
+			const { data } = await axios.post(
+				`${process.env.REACT_APP_BACKEND_URL}/activate`,
+				{ token },
+				{
+					// header:{
+					//     Authorization: `Bearer ${user.token}`,
+					// }
+				}
+			);
+			console.log("succesfull");
+			setSuccess(data.message);
+			Cookies.set("user", JSON.stringify({ ...user, verified: true }));
+			dispatch({
+				type: "VERIFY",
+				payload: true,
+			});
+			setTimeout(() => {
+				navigate("/");
+			}, 3000);
+			setLoading(false);
+		} catch (error) {
+			console.log(error.response.data);
+			setError(error.response.data.message);
+			setTimeout(() => {
+				navigate("/");
+			}, 3000);
+		}
+	};
+	useEffect(() => {
+		activateAccount();
+	}, []);
 	return (
-
 		<div className="home">
-            {
-                success && (
-                    <ActivateForm
-                    type="success"
-                    header="Account verification succeded"
-                    text={success}
-                    loading={loading}
-                />
-                )
-            }
-            {
-                error && (<ActivateForm
-                    type="error"
-                    header="Account verification failed"
-                    text={error}
-                    loading={loading}
-                />)
-            }
+			{success && (
+				<ActivateForm
+					type="success"
+					header="Account verification succeded"
+					text={success}
+					loading={loading}
+				/>
+			)}
+			{error && (
+				<ActivateForm
+					type="error"
+					header="Account verification failed"
+					text={error}
+					loading={loading}
+				/>
+			)}
 			<Header />
 			<LeftHome user={user} />
 			<div className="home_middle">

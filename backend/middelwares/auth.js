ff44b786850ea4b1a73250220fe54425f680d5d7
void-hr <gsharmaji93@gmail.com>
2023-07-28 23:19:25 +0530

    auth.js modified

--- backend/middelwares/auth.js
+++ backend/middelwares/auth.js
@@ -1,26 +1,5 @@
 const jwt = require("jsonwebtoken");
 
-<<<<<<< HEAD
-exports.authUser = async(req, res, next)=>{
-    try{
-        const token = tmp?tmp.slice(7, tmp.length):"";
-        if(!token){
-            return res.status(400).json({message: "Invalid Authentication"});
-        }
-        jwt.verify(token, process.env.TOKEN_SECRET,(err, user) => {
-            if(err){
-                return res.status(400).json({message:"Invalid Authentication"});
-                
-            }else{
-                req.user = user;
-                next();
-            }
-        })
-    }catch(error){
-        return res.status(500).json({message:error.message});
-    }
-}
-=======
 exports.authUser = async (req, res, next) => {
 	try {
 		let tmp = req.header("Authorization");
@@ -41,4 +20,3 @@ exports.authUser = async (req, res, next) => {
 		return res.status(500).json({ message: error.message });
 	}
 };
->>>>>>> 34c244ee49f49335887106571a5d66dc029aefbf

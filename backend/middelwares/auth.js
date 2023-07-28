402e69bf8dfc4213f7299dfb5e177aa30fb7661b
Akarsh711 <paliwalap7@gmail.com>
2023-07-28 22:35:51 +0530

    bug fixed

--- backend/middelwares/auth.js
+++ backend/middelwares/auth.js
@@ -2,7 +2,6 @@ const jwt = require("jsonwebtoken");
 
 exports.authUser = async(req, res, next)=>{
     try{
-        let tmp = req.header("Authorization");
         const token = tmp?tmp.slice(7, tmp.length):"";
         if(!token){
             return res.status(400).json({message: "Invalid Authentication"});
